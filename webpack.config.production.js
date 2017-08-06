c066e7848af2b13d31c4327c68a86d699c24f05e
phunkleho <kahoc@phm.gov.au>
2017-08-06 10:41:48 +1000

    Fixed webpack

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -46,6 +46,13 @@ module.exports = {
     }, {
       test: /\.svg$/,
       loader: "url?limit=10000&mimetype=image/svg+xml"
+    },
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "file-loader"
     }]
   }
 };
